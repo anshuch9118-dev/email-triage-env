54cc3d5da78ca86eb0141c756b889871432758d4
anshuch9118-dev <anshuch9118@gmail.com>
2026-04-08 22:24:53 +0530

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,15 @@
-from .graders import *
+from .environment import EmailEnvironment
+from .graders import (
+    classify_urgency_grader, 
+    choose_action_grader, 
+    draft_response_grader
+)
+
+# This ensures the decorators inside graders.py are executed 
+# the moment the 'server' package is touched by the validator.
+__all__ = [
+    "EmailEnvironment",
+    "classify_urgency_grader",
+    "choose_action_grader",
+    "draft_response_grader"
+]
