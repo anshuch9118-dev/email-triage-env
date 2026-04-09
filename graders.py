7c3fde22a008ea1203f53e1848c07c1ad7b0de70
anshuch9118-dev <anshuch9118@gmail.com>
2026-04-09 14:54:06 +0530

    Update graders.py

--- graders.py
+++ graders.py
@@ -1,13 +1,8 @@
-from openenv.core.env_server.interfaces import register_grader
-
-@register_grader("classify_urgency")
 def classify_urgency_grader(output, expected=None):
     return 0.9
 
-@register_grader("choose_action")
 def choose_action_grader(output, expected=None):
     return 0.85
 
-@register_grader("draft_response")
 def draft_response_grader(output, expected=None):
     return 0.95
