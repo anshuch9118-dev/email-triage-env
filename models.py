fd6de7957936125aa0d130b09cf4feeeca6dd070
anshuch9118-dev <anshuch9118@gmail.com>
2026-04-11 02:00:16 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class EmailAction(BaseModel):
     urgency: Optional[str] = ""
     action: Optional[str] = ""
     response_draft: Optional[str] = None
+
 
 class EmailObservation(BaseModel):
     email_subject: str = ""
