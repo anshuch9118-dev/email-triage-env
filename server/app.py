713e2e559f78168bfafb0efa6c3dc5401b18bae9
anshuch9118-dev <anshuch9118@gmail.com>
2026-04-11 16:43:08 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,3 +3,10 @@ from models import EmailAction, EmailObservation
 from server.environment import EmailEnvironment
 
 app = create_app(EmailEnvironment, EmailAction, EmailObservation, env_name="email_triage_env")
+
+def main():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
